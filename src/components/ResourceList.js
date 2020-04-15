46306563d39ef38efda46dc35d5761224078433a
Ryan Doherty <rjd.atx@gmail.com>
2020-04-14 23:51:55 -0500

    useEffect Lifecycle Implementation

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-class ResourceList extends React.Component {
-    state = { resources: [] };
+const ResourceList = ({ resource }) => {
+    const [resources, setResources] = useState([]);
 
-    async componentDidMount() {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-
-        this.setState({ resources: response.data });
+    const fetchResource = async (resource) => {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+        setResources(response.data);
     }
+
+    useEffect(() => {
+        fetchResource(resource);
+    }, [resource])
     
-    render() {
-        return <div>{this.state.resources.length}</div>;
-    }
+    return <div>{resources.length}</div>;
 }
 
 export default ResourceList;
