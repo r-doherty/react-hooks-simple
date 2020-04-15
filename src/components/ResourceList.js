7a0137a19d43fc084b1b5aae792eed98029224b5
Ryan Doherty <rjd.atx@gmail.com>
2020-04-15 00:02:53 -0500

    useEffect refactor

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -4,14 +4,20 @@ import axios from 'axios';
 const ResourceList = ({ resource }) => {
     const [resources, setResources] = useState([]);
 
-    const fetchResource = async (resource) => {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
-        setResources(response.data);
-    }
+    const fetchResource = 
 
-    useEffect(() => {
-        fetchResource(resource);
-    }, [resource])
+    useEffect(
+        () => {
+            (async (resource) => {
+                const response = await axios.get(
+                    `https://jsonplaceholder.typicode.com/${resource}`
+                );
+                
+                setResources(response.data);
+            })(resource);
+        }, 
+        [resource]
+    );
     
     return <div>{resources.length}</div>;
 }
